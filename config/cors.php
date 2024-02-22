9f2aace228380c992879491a4d6831ede581cb42
Solomon APPIER-SIGN <s.appier-sign@bsl.com.gh>
2024-02-22 23:01:51 +0000

    Update CORS paths in configuration
    The 'paths' configuration in 'cors.php' has been updated, specifically to include 'v1/*'. This update is important to ensure cross-origin resource sharing (CORS) works properly, by configuring the domain URLs that are permitted cross-site access.

--- config/cors.php
+++ config/cors.php
@@ -2,20 +2,7 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'v1/*'],
 
     'allowed_methods' => ['*'],
 
